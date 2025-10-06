6a5c97f6383577b236d2a5cffb3f65732956faf2
Diego Paredes <diegoparedesferro@gmail.com>
2025-10-06 18:31:48 -0300

    mi primer movimiento tp final programacion

---
+++ tpfinal.js
@@ -0,0 +1,6 @@
+//Control de Stock en una Tienda de Electrónica con Proveedores: Desarrolla una
+//aplicación que gestione el stock de una tienda de electrónica. Los productos tienen
+//nombre, marca, tipo (teléfono, laptop, etc.), cantidad disponible y proveedor.
+//Implementa un sistema de proveedores que suministran los productos. Permite buscar
+//productos por tipo, ver qué productos están en bajo stock y generar pedidos a los proveedores.
+
