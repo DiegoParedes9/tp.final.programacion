18e232ec20ba3f4e1a0e627ac747290fbb0457e7
florencia verona <veronaflorencia@gmail.com>
2025-10-30 17:05:55 -0300

    subo el archivo datos_Datos.js

---
+++ carga_Datos.js
@@ -0,0 +1,12 @@
+import fs from 'fs'
+const rutaDatos = "./productos.json";
+
+export function cargarDatos() {
+    const datos = JSON.parse(fs.readFileSync(rutaDatos, "utf-8"));
+    return datos;
+}
+
+export function guardarDatos(datos) {
+    const texto = JSON.stringify(datos, null, 2);
+    fs.writeFileSync(rutaAlumnos, texto, "utf-8");
+}
